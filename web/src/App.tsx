e9b4d6e9db9ae39d3cf96af8dc6e1c243406a9c7
peter <peter@debian.nothing>
2021-07-27 23:33:42 -0300

    add react scaffold

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Routes from './routes';
+import GlobalStyle from './styles/GlobalStyle';
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/styles/GlobalStyle.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  *, button, input {
+    background: none;
+    border: 0;
+    font-family: -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Ubuntu, Arial, sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
