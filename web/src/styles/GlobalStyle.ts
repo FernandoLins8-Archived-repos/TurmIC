7b462aea3cf4c8d2231659eb5da4ffe3e4246326
peter <peter@debian.nothing>
2021-07-28 00:16:34 -0300

    add global color variables

--- web/src/styles/GlobalStyle.ts
+++ web/src/styles/GlobalStyle.ts
@@ -16,4 +16,17 @@ export default createGlobalStyle`
   button {
     cursor: pointer;
   }
+
+  :root {
+    --gray-50: #F9FAFB;
+    --gray-100: #F3F4F6;
+    --gray-200: #E5E7EB;
+    --gray-300: #D1D5DB;
+    --gray-400: #9CA3AF;
+    --gray-500: #6B7280;
+    --gray-600: #4B5563;
+    --gray-700: #374151;
+    --gray-800: #1F2937;
+    --gray-900: #111827;
+  }
 `;
