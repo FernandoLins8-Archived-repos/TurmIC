bcd43d920108408402ae16fc637c373a8d2efed6
Fernando Lins <fernandolins088@gmail.com>
2021-08-05 21:36:59 -0300

    web: Fix Home (add ChangeEvent pkg)

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent, ChangeEvent } from 'react';
 import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
